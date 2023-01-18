66087a46051b728b8d5bd19cfd1f2a64bddf9ef1
sonamLama22 <lamadsonam19@gmail.com>
2023-01-18 15:45:21 -0500

    store messages in database

--- Server/src/models/users.js
+++ Server/src/models/users.js
@@ -6,6 +6,7 @@ const usersSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    message: { type: String, required: true },
   },
   { collection: "users" }
 );
