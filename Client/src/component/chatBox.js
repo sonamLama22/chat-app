2525e0b5b813b149bc370bbef2bb8ddaa1db8ecf
sonamLama22 <lamadsonam19@gmail.com>
2023-01-19 08:40:08 -0500

    test

--- Client/src/component/chatBox.js
+++ Client/src/component/chatBox.js
@@ -3,24 +3,49 @@ import "./chatBox.css";
 import { useSelector } from "react-redux";
 import "../App.js";
 
-const ChatBox = ({ selectedUser, socket }) => {
+const ChatBox = ({ selectedUser, socket, chat, setChat }) => {
   const { name } = useSelector((state) => state.user);
   // console.log(name);
   const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
+  // const [messageReceived, setMessageReceived] = useState("");
+  // const [chat, setChat] = useState([]); //chat is an array with long list of messages
+  // console.log(message);
+  //emits this event called "send_message" to the Server.
+  // const sendMessage = (e) => {
+  //   e.preventDefault();
+  //   socket.emit("send_message", { message });
 
-  const sendMessage = (e) => {
-    e.preventDefault();
-    socket.emit("send_message", { message }); //emits this event called "send_message" to the Server.
-  };
+  //   setMessage(""); //removes text from inputfield after submitting the text
+  // };
 
-  //listen to the event emitted by Server
+  //listen to the event "receiveMessage" emitted by Server
   //useEffect is called whenever a message is received.
-  useEffect(() => {
-    socket.on("receiveMessage", (data) => {
-      setMessageReceived(data.message);
-    });
-  }, [socket]);
+  // useEffect(() => {
+  //   socket.on("receiveMessage", (data) => {
+  //     console.log(data);
+  //     setChat([...chat, data]);
+  //   }); //listen to message from server
+  // }, [socket, data]);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ sender: name, message: message }),
+    };
+
+    const response = await fetch("http://localhost:4000/chat", requestOptions);
+    const data = await response.json();
+
+    if (data) {
+      alert("message sent");
+      // setChat(data.messages);
+      console.log(data);
+    }
+
+    setMessage("");
+  };
 
   return (
     <div className="chatbox">
@@ -42,19 +67,26 @@ const ChatBox = ({ selectedUser, socket }) => {
       </div>
       <div className="messageBox">
         <p className="chatMessage">
-          <span className="messageName">another user</span>
-          Message that a user receives
-          {messageReceived}
+          <span className="messageName">
+            {selectedUser[0].toUpperCase() + selectedUser.substring(1)}
+          </span>
+          {/* Message that a user receives */}
+          {message}
         </p>
+
         <p className="chatMessage receive">
-          <span className="messageName">{name} </span>
-          Message sent by the user
+          <span className="messageName">
+            {/* {name[0].toUpperCase() + name.substring(1)} */}
+            You
+          </span>
+          {/* Message sent by the user */}
           {message}
         </p>
       </div>
-      <div className="message__status">
+      {/* <div className="message__status">
         <p>Someone is typing...</p>
-      </div>
+      </div> */}
+
       <div className="messageInput">
         <form onSubmit={sendMessage}>
           <input
@@ -63,9 +95,7 @@ const ChatBox = ({ selectedUser, socket }) => {
             placeholder="Type a message"
             type="text"
           />
-          <button onClick={sendMessage} type="submit">
-            send
-          </button>
+          <button type="submit">send</button>
         </form>
       </div>
     </div>
--- Server/src/models/users.js
+++ Server/src/models/users.js
@@ -6,7 +6,6 @@ const usersSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    message: { type: String, required: true },
   },
   { collection: "users" }
 );
